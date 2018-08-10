173e396fc141ed4b2259b39d9e7c5aebbd509856
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2018-08-10 10:54:22 +0700

    init repo

---
+++ helper.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"strconv"
+)
+
+// ParseInt for parse string to integer
+func ParseInt(s string) (n int) {
+	n, _ = strconv.Atoi(s)
+	return
+}
+
+// ComputeHmac256 is
+func ComputeHmac256(s string) (res string) {
+	return
+}
